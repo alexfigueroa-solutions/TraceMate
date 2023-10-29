8a9f16bf2a9ced40efc2904da38e3fc76ae20983
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-29 13:21:38 -0700

    fix tests

--- tracemate/function_decorator.py
+++ tracemate/function_decorator.py
@@ -1,24 +1,60 @@
-
 import inspect
 import types
 from functools import wraps
 
 def logger_decorator(backend_logger, func):
-    async def wrapper(*args, **kwargs):
-        backend_logger.info(f"Entering {func.__name__}")
-        result = await func(*args, **kwargs)
-        backend_logger.info(f"Exiting {func.__name__}")
-        return result
-    return wrapper
+    if inspect.iscoroutinefunction(func):
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            backend_logger.info(f"Entering {func.__name__}")
+            result = await func(*args, **kwargs)
+            backend_logger.info(f"Exiting {func.__name__}")
+            return result
+        return async_wrapper
+    else:
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            backend_logger.info(f"Entering {func.__name__}")
+            result = func(*args, **kwargs)
+            backend_logger.info(f"Exiting {func.__name__}")
+            return result
+        return sync_wrapper
 
-def apply_decorator_to_all_functions_in_module(module, decorator):
+def apply_decorator_to_all_functions_in_module(module, backend_logger):
     for name, obj in inspect.getmembers(module):
         if inspect.isfunction(obj) or inspect.iscoroutinefunction(obj):
-            setattr(module, name, decorator(obj))
+            setattr(module, name, logger_decorator(backend_logger, obj))
 
-def apply_logger_to_all_functions(backend_logger):
-    current_module = types.ModuleType(__name__)
-    for attr_name, attr_value in globals().items():
-        if callable(attr_value):
-            decorated_func = logger_decorator(backend_logger, attr_value)
-            setattr(current_module, attr_name, decorated_func)
+# ... existing imports ...
+
+def logger_decorator(backend_logger, func):
+    print(f"Decorator being applied to {func.__name__}")  # Debugging line
+    if inspect.iscoroutinefunction(func):
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            print("Entering async decorated function")  # Debugging line
+            backend_logger.info(f"Entering {func.__name__}")
+            result = await func(*args, **kwargs)
+            backend_logger.info(f"Exiting {func.__name__}")
+            return result
+        return async_wrapper
+    else:
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            print("Entering sync decorated function")  # Debugging line
+            backend_logger.info(f"Entering {func.__name__}")
+            result = func(*args, **kwargs)
+            backend_logger.info(f"Exiting {func.__name__}")
+            return result
+        return sync_wrapper
+
+
+# ... existing imports ...
+
+def apply_logger_to_all_functions(backend_logger, functions_to_decorate):
+    decorated_functions = []
+    for func in functions_to_decorate:
+        decorated_func = logger_decorator(backend_logger, func)
+        globals()[func.__name__] = decorated_func
+        decorated_functions.append(decorated_func)
+    return decorated_functions
